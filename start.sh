2b75fbe7552e3bf467b6ab01237bcf03b24ea7eb
muruga <murugaemertxe1@gmail.com>
2026-03-11 14:03:46 +0530

    second commit

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 export LOGDIR=/var/log/airnotifier
-export LOGFILE=$LOGDIR/airnotifier.log
-export LOGFILE_ERR=$LOGDIR/airnotifier.err
 
 if [ ! -f "./config.py" ]; then
   cp config.py-sample config.py
@@ -15,17 +13,10 @@ if [ ! -f "./logging.ini" ]; then
   cp logging.ini-sample logging.ini
 fi
 
-sed -i "s/mongouri = \"mongodb:\/\/localhost:27017\/\"/mongouri = \"mongodb:\/\/${MONGO_SERVER-localhost}:${MONGO_PORT-27017}\"/g" ./config.py
-
-if [ ! -f "$LOGFILE" ]; then
-  touch "$LOGFILE"
-fi
-
-if [ ! -f "$LOGFILE_ERR" ]; then
-  touch "$LOGFILE_ERR"
-fi
+mkdir -p $LOGDIR
 
 echo "Installing AirNotifier ..."
-pipenv run ./install.py
-echo "Starting AirNotifier ..."
-pipenv run ./app.py >> "$LOGFILE" 2>> "$LOGFILE_ERR"
+pipenv run python install.py
+
+echo "Starting AirNotifier on port ${PORT:-10000} ..."
+pipenv run python app.py --port=${PORT:-10000}
